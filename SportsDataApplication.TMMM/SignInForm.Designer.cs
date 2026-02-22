cd1028ed4afdd25c07978480803260170e153e10
CalebTucker26 <caleb.d.tucker@gmail.com>
2026-02-21 23:13:26 -0500

    Sign In Form Operational
    Added the Sign In Database. Coded the logic for user creation.

--- SportsDataApplication.TMMM/SignInForm.Designer.cs
+++ SportsDataApplication.TMMM/SignInForm.Designer.cs
@@ -28,21 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label userNameLabel;
+            System.Windows.Forms.Label passwordLabel;
+            this.sign_InDataSet = new SportsDataApplication.TMMM.Sign_InDataSet();
+            this.credentialsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.credentialsTableAdapter = new SportsDataApplication.TMMM.Sign_InDataSetTableAdapters.CredentialsTableAdapter();
+            this.tableAdapterManager = new SportsDataApplication.TMMM.Sign_InDataSetTableAdapters.TableAdapterManager();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            userNameLabel = new System.Windows.Forms.Label();
+            passwordLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sign_InDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.credentialsBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // sign_InDataSet
+            // 
+            this.sign_InDataSet.DataSetName = "Sign_InDataSet";
+            this.sign_InDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // credentialsBindingSource
+            // 
+            this.credentialsBindingSource.DataMember = "Credentials";
+            this.credentialsBindingSource.DataSource = this.sign_InDataSet;
+            // 
+            // credentialsTableAdapter
+            // 
+            this.credentialsTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.CredentialsTableAdapter = this.credentialsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = SportsDataApplication.TMMM.Sign_InDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // userNameLabel
+            // 
+            userNameLabel.AutoSize = true;
+            userNameLabel.Location = new System.Drawing.Point(216, 163);
+            userNameLabel.Name = "userNameLabel";
+            userNameLabel.Size = new System.Drawing.Size(63, 13);
+            userNameLabel.TabIndex = 3;
+            userNameLabel.Text = "User Name:";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.credentialsBindingSource, "UserName", true));
+            this.userNameTextBox.Location = new System.Drawing.Point(285, 160);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userNameTextBox.TabIndex = 4;
+            // 
+            // passwordLabel
+            // 
+            passwordLabel.AutoSize = true;
+            passwordLabel.Location = new System.Drawing.Point(216, 189);
+            passwordLabel.Name = "passwordLabel";
+            passwordLabel.Size = new System.Drawing.Size(56, 13);
+            passwordLabel.TabIndex = 5;
+            passwordLabel.Text = "Password:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.credentialsBindingSource, "Password", true));
+            this.passwordTextBox.Location = new System.Drawing.Point(285, 186);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 6;
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.Location = new System.Drawing.Point(215, 307);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(82, 47);
+            this.btnCreateUser.TabIndex = 7;
+            this.btnCreateUser.Text = "Create User";
+            this.btnCreateUser.UseVisualStyleBackColor = true;
+            this.btnCreateUser.Click += new System.EventHandler(this.btnCreateUser_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(303, 307);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(82, 47);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // SignInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCreateUser);
+            this.Controls.Add(userNameLabel);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(passwordLabel);
+            this.Controls.Add(this.passwordTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "SignInForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sign In - Sports Data Application";
+            this.Load += new System.EventHandler(this.SignInForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sign_InDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.credentialsBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Sign_InDataSet sign_InDataSet;
+        private System.Windows.Forms.BindingSource credentialsBindingSource;
+        private Sign_InDataSetTableAdapters.CredentialsTableAdapter credentialsTableAdapter;
+        private Sign_InDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button btnCreateUser;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
