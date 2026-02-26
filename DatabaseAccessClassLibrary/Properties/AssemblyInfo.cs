ade445619d19f550a432a6bc852a280004e1b0d1
Jason Moore <json.mooreb@gmail.com>
2026-02-25 22:39:40 -0500

    Added Class Library
    Added class library and references to forms. Coding not complete yet.

--- DatabaseAccessClassLibrary/Properties/AssemblyInfo.cs
+++ DatabaseAccessClassLibrary/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("31b50b06-d4f0-45bd-a6ea-35ac2d8efaac")]
+[assembly: Guid("aacc3a4c-23bf-4f21-856c-0c9ebcb3b935")]
 
 // Version information for an assembly consists of the following four values:
 //
