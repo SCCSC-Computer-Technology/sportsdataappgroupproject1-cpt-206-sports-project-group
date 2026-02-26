1b3a9e814a3614e0a93b9703c8ad7199d8232fa3
Jason Moore <json.mooreb@gmail.com>
2026-02-25 22:06:57 -0500

    Add class library

---
+++ DatabaseAccessClassLibrary/DataAccessLibrary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Created a class library to be able to access the database files from within the debug folder
+// Library contains connection strings for all databases
+namespace DatabaseAccessClassLibrary
+{
+    public class DataAccessLibrary
+    {
+
+    }
+}
