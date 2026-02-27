eb70d01f7b0479bca871f7cded0683ab509411cb
Jason Moore <json.mooreb@gmail.com>
2026-02-26 22:27:31 -0500

    Update button logic
    Added nav buttons, removed data table editing buttons. Users should only be able to view data only.

--- SportsDataApplication.TMMM/NFL_UpcomingGames_MatchResults.Designer.cs
+++ SportsDataApplication.TMMM/NFL_UpcomingGames_MatchResults.Designer.cs
@@ -35,9 +35,7 @@
             this.credentialsTableAdapter = new SportsDataApplication.TMMM.Sign_InDataSetTableAdapters.CredentialsTableAdapter();
             this.tableAdapterManager = new SportsDataApplication.TMMM.Sign_InDataSetTableAdapters.TableAdapterManager();
             this.credentialsBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
-            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
-            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
@@ -46,7 +44,6 @@
             this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
             this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-            this.credentialsBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
             this.group_Project_1_Sports_DatabaseDataSet = new SportsDataApplication.TMMM.Group_Project_1_Sports_DatabaseDataSet();
             this.nFL_Upcoming_GamesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nFL_Upcoming_GamesTableAdapter = new SportsDataApplication.TMMM.Group_Project_1_Sports_DatabaseDataSetTableAdapters.NFL_Upcoming_GamesTableAdapter();
@@ -58,6 +55,8 @@
             this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sign_InDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.credentialsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.credentialsBindingNavigator)).BeginInit();
@@ -65,6 +64,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.group_Project_1_Sports_DatabaseDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nFL_Upcoming_GamesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nFL_Upcoming_GamesDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // sign_InDataSet
@@ -89,10 +89,10 @@
             // 
             // credentialsBindingNavigator
             // 
-            this.credentialsBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.credentialsBindingNavigator.AddNewItem = null;
             this.credentialsBindingNavigator.BindingSource = this.credentialsBindingSource;
             this.credentialsBindingNavigator.CountItem = this.bindingNavigatorCountItem;
-            this.credentialsBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.credentialsBindingNavigator.DeleteItem = null;
             this.credentialsBindingNavigator.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.credentialsBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.bindingNavigatorMoveFirstItem,
@@ -103,10 +103,7 @@
             this.bindingNavigatorSeparator1,
             this.bindingNavigatorMoveNextItem,
             this.bindingNavigatorMoveLastItem,
-            this.bindingNavigatorSeparator2,
-            this.bindingNavigatorAddNewItem,
-            this.bindingNavigatorDeleteItem,
-            this.credentialsBindingNavigatorSaveItem});
+            this.bindingNavigatorSeparator2});
             this.credentialsBindingNavigator.Location = new System.Drawing.Point(0, 0);
             this.credentialsBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
             this.credentialsBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
@@ -118,30 +115,12 @@
             this.credentialsBindingNavigator.TabIndex = 0;
             this.credentialsBindingNavigator.Text = "bindingNavigator1";
             // 
-            // bindingNavigatorAddNewItem
-            // 
-            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
-            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
-            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
-            this.bindingNavigatorAddNewItem.Text = "Add new";
-            // 
             // bindingNavigatorCountItem
             // 
             this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
             this.bindingNavigatorCountItem.Size = new System.Drawing.Size(45, 24);
             this.bindingNavigatorCountItem.Text = "of {0}";
             this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
-            // 
-            // bindingNavigatorDeleteItem
-            // 
-            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
-            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
-            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
-            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
-            this.bindingNavigatorDeleteItem.Text = "Delete";
             // 
             // bindingNavigatorMoveFirstItem
             // 
@@ -204,15 +183,6 @@
             this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
             this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
             // 
-            // credentialsBindingNavigatorSaveItem
-            // 
-            this.credentialsBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.credentialsBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("credentialsBindingNavigatorSaveItem.Image")));
-            this.credentialsBindingNavigatorSaveItem.Name = "credentialsBindingNavigatorSaveItem";
-            this.credentialsBindingNavigatorSaveItem.Size = new System.Drawing.Size(29, 24);
-            this.credentialsBindingNavigatorSaveItem.Text = "Save Data";
-            this.credentialsBindingNavigatorSaveItem.Click += new System.EventHandler(this.credentialsBindingNavigatorSaveItem_Click);
-            // 
             // group_Project_1_Sports_DatabaseDataSet
             // 
             this.group_Project_1_Sports_DatabaseDataSet.DataSetName = "Group_Project_1_Sports_DatabaseDataSet";
@@ -243,11 +213,11 @@
             this.dataGridViewTextBoxColumn3,
             this.dataGridViewTextBoxColumn4});
             this.nFL_Upcoming_GamesDataGridView.DataSource = this.nFL_Upcoming_GamesBindingSource;
-            this.nFL_Upcoming_GamesDataGridView.Location = new System.Drawing.Point(12, 341);
+            this.nFL_Upcoming_GamesDataGridView.Location = new System.Drawing.Point(26, 321);
             this.nFL_Upcoming_GamesDataGridView.Name = "nFL_Upcoming_GamesDataGridView";
             this.nFL_Upcoming_GamesDataGridView.RowHeadersWidth = 51;
             this.nFL_Upcoming_GamesDataGridView.RowTemplate.Height = 24;
-            this.nFL_Upcoming_GamesDataGridView.Size = new System.Drawing.Size(557, 220);
+            this.nFL_Upcoming_GamesDataGridView.Size = new System.Drawing.Size(734, 220);
             this.nFL_Upcoming_GamesDataGridView.TabIndex = 1;
             // 
             // dataGridViewTextBoxColumn1
@@ -285,26 +255,50 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.Size = new System.Drawing.Size(170, 18);
             this.label1.TabIndex = 2;
             this.label1.Text = "NFL - Upcoming Games";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 313);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 300);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.Size = new System.Drawing.Size(144, 18);
             this.label2.TabIndex = 3;
             this.label2.Text = "NFL - Match Results";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(26, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 185);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(708, 596);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(92, 35);
+            this.btnBack.TabIndex = 5;
+            this.btnBack.Text = "&Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // NFL_UpcomingGames_MatchResults
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 573);
+            this.ClientSize = new System.Drawing.Size(812, 643);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nFL_Upcoming_GamesDataGridView);
@@ -320,6 +314,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.group_Project_1_Sports_DatabaseDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nFL_Upcoming_GamesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nFL_Upcoming_GamesDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -332,9 +327,7 @@
         private Sign_InDataSetTableAdapters.CredentialsTableAdapter credentialsTableAdapter;
         private Sign_InDataSetTableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.BindingNavigator credentialsBindingNavigator;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
         private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
-        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
         private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
         private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
         private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
@@ -343,7 +336,6 @@
         private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
         private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
         private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
-        private System.Windows.Forms.ToolStripButton credentialsBindingNavigatorSaveItem;
         private Group_Project_1_Sports_DatabaseDataSet group_Project_1_Sports_DatabaseDataSet;
         private System.Windows.Forms.BindingSource nFL_Upcoming_GamesBindingSource;
         private Group_Project_1_Sports_DatabaseDataSetTableAdapters.NFL_Upcoming_GamesTableAdapter nFL_Upcoming_GamesTableAdapter;
@@ -355,5 +347,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnBack;
     }
 }
--- SportsDataApplication.TMMM/NFL_UpcomingGames_MatchResults.cs
+++ SportsDataApplication.TMMM/NFL_UpcomingGames_MatchResults.cs
@@ -18,21 +18,15 @@ namespace SportsDataApplication.TMMM
             InitializeComponent();
         }
 
-        private void credentialsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.credentialsBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.sign_InDataSet);
-
-        }
-
         private void NFLUpcomingGames_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'group_Project_1_Sports_DatabaseDataSet.NFL_Upcoming_Games' table. You can move, or remove it, as needed.
-            this.nFL_Upcoming_GamesTableAdapter.Fill(this.group_Project_1_Sports_DatabaseDataSet.NFL_Upcoming_Games);
-            // TODO: This line of code loads data into the 'sign_InDataSet.Credentials' table. You can move, or remove it, as needed.
-            this.credentialsTableAdapter.Fill(this.sign_InDataSet.Credentials);
+            // Load form data form database
+        }
 
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Close Upcoming Games / Match Results form
+            this.Close();
         }
     }
 }
