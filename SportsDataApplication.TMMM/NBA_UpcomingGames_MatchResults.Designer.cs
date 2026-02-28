19efbcfef79e1d46cbc457d4009e10144f5010b1
Jason Moore <json.mooreb@gmail.com>
2026-02-28 00:01:05 -0500

    Update NBA_UpcomingGames_MatchResults.Designer.cs

--- SportsDataApplication.TMMM/NBA_UpcomingGames_MatchResults.Designer.cs
+++ SportsDataApplication.TMMM/NBA_UpcomingGames_MatchResults.Designer.cs
@@ -49,8 +49,6 @@
             this.comboBoxTeamSort = new System.Windows.Forms.ComboBox();
             this.btnMonthSort = new System.Windows.Forms.Button();
             this.comboBoxMonthSort = new System.Windows.Forms.ComboBox();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.group_Project_1_Sports_DatabaseDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nBA_Upcoming_GamesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nBA_Upcoming_GamesBindingNavigator)).BeginInit();
             this.nBA_Upcoming_GamesBindingNavigator.SuspendLayout();
