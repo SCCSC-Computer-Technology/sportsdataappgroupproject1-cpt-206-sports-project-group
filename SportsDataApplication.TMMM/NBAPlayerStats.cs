6fb695b9f1d2c85a4a6c4f132d408e0bbc3380bb
Jason Moore <json.mooreb@gmail.com>
2026-03-02 22:55:31 -0500

    Revert commit

--- SportsDataApplication.TMMM/NBAPlayerStats.cs
+++ SportsDataApplication.TMMM/NBAPlayerStats.cs
@@ -1,5 +1,4 @@
-﻿using SportsDataApplication.TMMM.SportsProjectDBDataSetTableAdapters;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,41 +21,6 @@ namespace SportsDataApplication.TMMM
         {
             // close NBA player stats form
             this.Close();
-        }
-
-        private void NBAPlayerStats_Load(object sender, EventArgs e)
-        {
-            // TODO: This line of code loads data into the 'sportsProjectDBDataSet.nba_playerstats_2024' table. You can move, or remove it, as needed.
-            this.nba_playerstats_2024TableAdapter.Fill(this.sportsProjectDBDataSet.nba_playerstats_2024);
-            nba_playerstats_2024DataGridView.DataError += dataGridView1_DataError;
-        }
-
-
-        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
-        {
-            e.ThrowException = false;
-        }
-
-        private void nba_playerstats_2024BindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.nba_playerstats_2024BindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.sportsProjectDBDataSet);
-
-        }
-
-        private void nba_playerstats_2024BindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.nba_playerstats_2024BindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.sportsProjectDBDataSet);
-
-        }
-
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-            string playerName=textBox1.Text;
-            nba_playerstats_2024TableAdapter.FillBySearchPlayer(sportsProjectDBDataSet.nba_playerstats_2024, playerName);
         }
     }
 }
